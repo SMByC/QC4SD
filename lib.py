c238c084673f31b0a9c0816febda0ab72ee2ac4d
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2015-12-06 00:12:35 -0500

    Added repulsive_items_list to lib, this function separate the items in list if this have less of repulsive distance value and separate its elements with the minimum distance.

--- lib.py
+++ lib.py
@@ -127,3 +127,91 @@ def frange(start, stop, step):
             break
         L.append(next)
     return L
+
+
+def repulsive_items_list(l, repulsive_distance=None, min_distance=None):
+    """Separate the items in list if this have less of repulsive distance
+    value and separate its elements with the minimum distance.
+
+    :Example:
+
+        >>> l=[1,2,4,10,23,25]
+        >>> repulsive_items_list(l, 3, 3)
+        [-0.5, 2.5, 5.5, 10, 22.5, 25.5]
+
+    :param l: list to process
+    :type l: list
+    :param repulsive_distance: minimum value for check the distance between items
+    :type repulsive_distance: float
+    :param min_distance: minimum distance for separate the items
+    :type min_distance: float
+    :rtype: list
+    """
+    len_list = max(l)-min(l)
+    if repulsive_distance is None:
+        repulsive_distance = len_list*0.028
+    if min_distance is None:
+        min_distance = repulsive_distance
+
+    ###### group list
+    l_sort = sorted(list(zip(range(len(l)), l)), key=lambda x: x[1])
+    d = dict(l_sort)
+
+    items_to_fix = []
+    for idx, (k, v) in enumerate(l_sort[0:-1]):
+        if abs(v - l_sort[idx+1][1]) < repulsive_distance:
+            items_to_fix.append([k, l_sort[idx+1][0]])
+    if not items_to_fix:
+        return False, repulsive_distance, l
+
+    join_groups = []
+    group_joined = False
+    for group0, group1 in zip(items_to_fix[0:-1], items_to_fix[1::]):
+        if group0[-1] == group1[0]:
+            if group_joined:
+                to_remove = join_groups[-1]
+                join_groups.append(join_groups[-1] + group1[1::])
+                join_groups.remove(to_remove)
+            else:
+                join_groups.append(group0 + group1[1::])
+            group_joined = True
+            if group0 in join_groups:
+                join_groups.remove(group0)
+        else:
+            if group_joined:
+                join_groups.append(group1)
+            elif group0 in join_groups:
+                join_groups.append(group1)
+            else:
+                join_groups.append(group0)
+                if group1 == items_to_fix[-1]:
+                    join_groups.append(group1)
+            group_joined = False
+    if len(items_to_fix) == 1 and not join_groups:
+        join_groups = items_to_fix
+
+    ###### fixes list
+    def fix_group_distance(group):
+        min_value = d[group[0]]
+        max_value = d[group[-1]]
+        center_value = min_value + (max_value-min_value)/2.0
+        if len(group) % 2 == 0:  # even
+            side = len(group)*min_distance/2.0 - min_distance/2.0
+        else:  # odd
+            side = (len(group)-1)*min_distance/2.0
+        re_adjust = frange(center_value-side, center_value+side+1, min_distance)
+        for item in group:
+            pass
+        return re_adjust
+
+    for group in join_groups:
+        group_fixes = fix_group_distance(group)
+        for idx, item in enumerate(group):
+            d[item] = group_fixes[idx]
+
+    result = [round(x[1], 3) for x in sorted(d.items(), key=lambda x: x[0])]
+    if result == l:
+        return False, repulsive_distance, result
+    else:
+        return True, repulsive_distance, result
+
