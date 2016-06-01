6379e90643c8b5bb6518cdb3ca4663b70f7ef812
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2016-06-01 13:53:45 -0500

    Fix repulsive_items_list function when the items are near and not are in the real range 0 to max

--- qc4sd/lib.py
+++ qc4sd/lib.py
@@ -147,9 +147,9 @@ def repulsive_items_list(l, repulsive_distance=None, min_distance=None):
     :type min_distance: float
     :rtype: list
     """
-    len_list = max(l)-min(l)
+
     if repulsive_distance is None:
-        repulsive_distance = len_list*0.04
+        repulsive_distance = max(l)*0.04
     else:
         repulsive_distance -= repulsive_distance*0.01
     if min_distance is None:
